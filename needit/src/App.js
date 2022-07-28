3fb85037cddc85aae1c1ce1c7074c4138ee5af5b
Asaadlink <asaadfit@gmail.com>
2022-07-28 15:54:59 +0200

    adding the new logo

---
+++ needit/src/App.js
@@ -0,0 +1,23 @@
+import logo from './img/globe-recycling-icon-recycling-earth.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <a
+          className="App-link"
+          href="#"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Need It ?
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
