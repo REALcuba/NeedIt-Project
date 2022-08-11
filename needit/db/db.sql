3276d0022a81a0c4cb89f26f14615e1049ff4455
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-08-11 07:45:21 +0200

    basic db layout with some entry data

--- needit/db/db.sql
+++ needit/db/db.sql
@@ -16,7 +16,7 @@ insert into users (username, email, password, address, postcode, phone, bio) val
 CREATE TABLE listings (
   id        SERIAL PRIMARY KEY,
   user_id      int references users(id),
-  title     VARCHAR(120) NOT NULL,
+  title     VARCHAR(360) NOT NULL,
   itemsqty int,
   picture bytea,
   location varchar(120),
@@ -26,3 +26,24 @@ CREATE TABLE listings (
 );
 
 insert into listings (user_id, title, total_items, location, postcode, status, description) values (3, 'Couch bed', 1, 'Passeig de Gracia', '08008', 2, 'A regular couch that turns into a bed. One of the springs collapsed, and the bar in the centre is kind of uncomfortable on your back, but you are still getting more than you paid for' )
+
+create table transactions (
+id        SERIAL PRIMARY KEY,
+listing_id int references listings(id),
+publisher_id int references users(id),
+receiver_id int references users(id),
+exchange_date date
+);
+
+insert into transactions (listing_id, publisher_id, receiver_id, exchange_date) values (6, 2, 4, '2022/11/08');
+insert into transactions (listing_id, publisher_id, receiver_id, exchange_date) values (3, 1, 5, '2022/11/08');
+
+create table reviews (
+id        SERIAL PRIMARY KEY,
+listing_id int references listings(id),
+publisher_id int references users(id),
+receiver_id int references users(id),
+message varchar(1200),
+rating int )
+
+insert into reviews (listing_id, publisher_id, receiver_id, message, rating) values (3, 1, 5, 'This couch looks really nice, but its killing my back', 3);
