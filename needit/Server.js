f33a1665a05bd0eb36818c0bdedef6a2666113d5
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-08-12 08:09:49 +0200

    added list of endpoints on file

--- needit/Server.js
+++ needit/Server.js
@@ -54,6 +54,22 @@ app.get("/listings/:category", function (req, res) {
     .catch((e) => console.error(e));
 });
 
+/* missing endpoints (without chat)
+listings/:category
+listings/:title(includes method, turn to lowercase)
+listings/:postcode
+listings/:date (I'll modify the DB to add this row)
+transactions
+transactions/:id
+transactions/:publisher_Id
+transactions/:receiver_Id
+transactions/:date
+reviews/:id
+reviews/:publisher_id
+reviews/:receiver_id
+reviews/receiver_id/:rating
+*/
+
 app.listen(port, () => {
   console.log(`needIt app listening on port ${port}`);
 });
