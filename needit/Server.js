12ff4b9d823de91d6c457ef4b35b93658ff7043c
vgl-bwk <83692259+vgl-bwk@users.noreply.github.com>
2022-08-12 07:56:04 +0200

    added server file with endpoints for users, users by username, listings, listings by ID and commentary on the necesary endpoints

---
+++ needit/Server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const port = 3000;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "needIt",
+  password: "",
+  port: 5432,
+});
+
+app.use(bodyParser.json());
+
+app.get("/users", function (req, res) {
+  pool.query("SELECT * FROM users", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.get("/users/:username", function (req, res) {
+  const userName = req.params.username;
+
+  pool
+    .query(`SELECT * FROM users WHERE users.username=$1`, [userName])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/listings", function (req, res) {
+  pool.query("SELECT * FROM listings", (error, result) => {
+    res.json(result.rows);
+  });
+});
+
+app.get("/listings/:listingId", function (req, res) {
+  const listingId = req.params.listingId;
+
+  pool
+    .query(`SELECT * FROM listings WHERE listings.id=$1`, [listingId])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.get("/listings/:category", function (req, res) {
+  const category = req.params.category;
+
+  pool
+    .query(`SELECT * FROM listings WHERE listings.category=$1`, [category])
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e));
+});
+
+app.listen(port, () => {
+  console.log(`needIt app listening on port ${port}`);
+});
