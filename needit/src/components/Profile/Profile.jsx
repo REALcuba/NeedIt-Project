f27e9855c793e335fa73b35356dae0cba6895590
REALcuba <angelskcuba@gmail.com>
2022-08-18 20:09:05 +0200

    updating profile pic

--- needit/src/components/Profile/Profile.jsx
+++ needit/src/components/Profile/Profile.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 
+
+//style
+import './profile'
+import blank from '../../img/blank.jpg'
 function Profile() {
   return (
     <div className='userProfile'>
         <div className='profileContentDiv'>
             <div>
+                <div className='profileImgDiv'>
+                    <img src={blank} alt="" />
+                </div>
                 <div>
 
-                </div>
                 Profile
+                </div>
             </div>
             <div>bio</div>
             <div>user products</div>
