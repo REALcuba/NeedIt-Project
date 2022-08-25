ced8fbd58d04e203a649723b32fd89af68891284
REALcuba <angelskcuba@gmail.com>
2022-08-25 12:16:05 +0200

    Refactoring css with bootstrap

--- needit/src/components/Header/Header.jsx
+++ needit/src/components/Header/Header.jsx
@@ -8,40 +8,68 @@ import SignUpBtn from "../SignUpBtn/SignUpBtn";
 
 // import MenuBtn from "../MenuBtn/MenuBtn";
 import LoginBtn from "../LoginBtn/LoginBtn";
-import ProductPageBtn from "../ProductPageBtn/ProductPageBtn";
+// import ProductPageBtn from "../ProductPageBtn/ProductPageBtn";
 
 function Header() {
   return (
     <div>
-      <div className="navbar container-fluid">
+      <div className="navbar container">
         <div className=" navbar-brand">
           <Link to="/">
-            <img src={main_logo} className="main_logo navbar-brand" alt="logo" />
+            <img
+              src={main_logo}
+              className="main_logo navbar-brand"
+              alt="logo"
+            />
           </Link>
-        </div >
-        
-        <ul className="navbar-nav navBar_wrapper">
-          <li className="nav-item"><Link to="/login" className="nav-link">
-            <LoginBtn />
-          </Link></li>
+        </div>
+        <div>
+          <ul className="navbar-nav navBar_wrapper">
+            <li className="nav-item">
+              <Link to="/login" className="nav-link">
+                <LoginBtn />
+              </Link>
+            </li>
 
-          <li className="nav-item"><Link to="/register">
-            <SignUpBtn />
-          </Link></li>
+            <li className="nav-item">
+              <Link to="/register">
+                <SignUpBtn />
+              </Link>
+            </li>
+          </ul>
+        </div>
 
-          {/* <MenuBtn/> */}
+        {/* <MenuBtn/> */}
 
-          {/* <li className="nav-item"><Link to="/product">
+        {/* <li className="nav-item"><Link to="/product">
             <ProductPageBtn />
           </Link></li> */}
-          <li class="nav-item dropstart"/>
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#">Menu</a>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Link 1</a></li>
-      <li><a class="dropdown-item" href="#">Link 2</a></li>
-      <li><a class="dropdown-item" href="#">Link 3</a></li>
-        </ul>
-       </ul>
+        <div>
+          <h3
+            className="nav-link dropdown-toggle "
+            data-bs-toggle="dropdown"
+           
+          >
+            Menu
+          </h3>
+          <ul class="dropdown-menu dropdown-menu-end">
+            <li>
+              <Link className="dropdown-item" to="/profile">
+                Profile
+              </Link>
+            </li>
+            <li>
+              <Link className="dropdown-item" to="product">
+                product
+              </Link>
+            </li>
+            <li>
+              <Link className="dropdown-item" to="/about">
+                About Us
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- needit/src/components/Profile/Profile.jsx
+++ needit/src/components/Profile/Profile.jsx
@@ -3,23 +3,12 @@ import React from "react";
 //style
 import "./profile.css";
 import blank from "../../img/blank.jpg";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 function Profile() {
   return (
     
     <div className="userProfileBody container">
-      {/* <div class="dropdown">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
-    Dropdown button
-  </button>
-  <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-    <Link to="/profile"><li class="dropdown-item" >Profile</li></Link>
-    <Link><li class="dropdown-item active" >Products</li></Link>
-    <li class="dropdown-item" >Something else here</li>
-    <li><hr class="dropdown-divider"/></li>
-    <li class="dropdown-item" >Separated link</li>
-  </ul>
-</div> */}
+     
       <div className="profileContentDiv">
         <div className="profileDetails">
           <div className="imgDiv">
