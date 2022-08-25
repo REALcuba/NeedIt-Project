deaec484f46952be830deb4481f330159092aa0b
REALcuba <angelskcuba@gmail.com>
2022-08-25 14:31:18 +0200

    dropdown menu done

--- needit/src/components/Header/Header.jsx
+++ needit/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-
+import menuImg from "../../img/casa.png";
 import "./header.css";
 import main_logo from "../../img/main_logo.png";
 import SignUpBtn from "../SignUpBtn/SignUpBtn";
@@ -12,66 +12,62 @@ import LoginBtn from "../LoginBtn/LoginBtn";
 
 function Header() {
   return (
-    <div>
-      <div className="navbar container">
-        <div className=" navbar-brand">
-          <Link to="/">
-            <img
-              src={main_logo}
-              className="main_logo navbar-brand"
-              alt="logo"
-            />
-          </Link>
-        </div>
-        <div>
-          <ul className="navbar-nav navBar_wrapper">
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">
-                <LoginBtn />
-              </Link>
-            </li>
+    // <div className="container">
+    <div className="navbar ">
+      <div className=" navbar-brand">
+        <Link to="/">
+          <img src={main_logo} className="main_logo navbar-brand" alt="logo" />
+        </Link>
+      </div>
+      <div>
+        <ul className="navbar-nav navBar_wrapper">
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              <LoginBtn />
+            </Link>
+          </li>
 
-            <li className="nav-item">
-              <Link to="/register">
-                <SignUpBtn />
-              </Link>
-            </li>
-          </ul>
-        </div>
+          <li className="nav-item">
+            <Link to="/register">
+              <SignUpBtn />
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-        {/* <MenuBtn/> */}
+      {/* <MenuBtn/> */}
 
-        {/* <li className="nav-item"><Link to="/product">
+      {/* <li className="nav-item"><Link to="/product">
             <ProductPageBtn />
           </Link></li> */}
-        <div>
-          <h3
-            className="nav-link dropdown-toggle "
-            data-bs-toggle="dropdown"
-           
-          >
-            Menu
-          </h3>
-          <ul class="dropdown-menu dropdown-menu-end">
-            <li>
-              <Link className="dropdown-item" to="/profile">
-                Profile
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="product">
-                product
-              </Link>
-            </li>
-            <li>
-              <Link className="dropdown-item" to="/about">
-                About Us
-              </Link>
-            </li>
-          </ul>
+      <div className="menuIcon">
+        <div
+          
+          className="nav-link dropdown-toggle "
+          data-bs-toggle="dropdown"
+        >
+          <img src={menuImg}  className="menuImg" alt="menu" />
         </div>
+        <ul class="dropdown-menu dropdown-menu-end">
+          <li>
+            <Link className="dropdown-item" to="/profile">
+              Profile
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="product">
+              product
+            </Link>
+          </li>
+          <li>
+            <Link className="dropdown-item" to="/about">
+              About Us
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
+    // </div>
   );
 }
 
