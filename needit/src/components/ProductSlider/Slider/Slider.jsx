cdd814d3f388a698df721cb45186636943198f3e
REALcuba <angelskcuba@gmail.com>
2022-08-26 19:02:07 +0200

    refactoring slider css eith bootstrap

--- needit/src/components/ProductSlider/Slider/Slider.jsx
+++ needit/src/components/ProductSlider/Slider/Slider.jsx
@@ -28,7 +28,7 @@ export default function Slider() {
   
     <div className=" container-slider container-fluid">
       {dataSlider.map((item) => (
-        <div className="categorySlider row" key={item.id}>
+        <div className="categorySlider" key={item.id}>
           <div className="row-sm-5">
             <h2>{item.name}</h2>
             <img className="cardImg  " src={item.img} alt={item.name} />
