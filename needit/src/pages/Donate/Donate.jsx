fbf1d9d1e391c003874c2c4d139aabd32446762d
REALcuba <angelskcuba@gmail.com>
2022-09-02 12:36:49 +0200

    donate form 90% done

--- needit/src/pages/Donate/Donate.jsx
+++ needit/src/pages/Donate/Donate.jsx
@@ -1,42 +1,91 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Donate() {
-    const [emailReg, setEmailReg] = useState("");
+//   const [productInfo, setProductInfo] = useState("");
+  const [product, setProduct] = useState("");
+  const [status, setStatus] = useState("");
+  const [description, setDescription] = useState("");
+  const [cathegory, setCathegory] = useState("");
+  const [Pictures, setPictures] = useState([]);
 
-    const [passwordReg, setpasswordReg] = useState("");
+  // const handleChange =(event) =>{
+  //     setProductInfo(event.target.value);
+  //     console.log({setProductInfo});
+  // }
+
   return (
-    <div className='container'>
-        <form className="loginmember d-flex flex-column" onSubmit={Donate}>
+    <div className="container">
+      <form className="loginmember d-flex flex-column">
         <h1>Donate your product</h1>
-
+        <label for="product" class="form-label">
+          Nombre del producto:
+        </label>
         <input
-        className="form-control"
-          type="email"
-          placeholder="Email"
+          className="form-control"
+          type="title"
+          placeholder="Nombre del producto"
           required
           onChange={(e) => {
-            setEmailReg(e.target.value);
+            setProduct(e.target.value);
           }}
+        //   value={productInfo}
         ></input>
-
+<label for="status" class="form-label">
+          Status:
+        </label>
         <input
-        className="form-control"
-          type="password"
-          placeholder="password"
+          className="form-control"
+          type="status"
+          placeholder="Status"
           required
           onChange={(e) => {
-            setpasswordReg(e.target.value);
+            setStatus(e.target.value);
           }}
         ></input>
+        <label for="cathegory" class="form-label">
+          Categoria:
+        </label>
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Categoria"
+          required
+          onChange={(e) => {
+            setCathegory(e.target.value);
+          }}
+        ></input>
+        <label for="description" class="form-label">
+          Descripción:
+        </label>
+        <input
+          className="form-control"
+          type="description"
+          placeholder="descripción"
+          required
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        ></input>
+        <label for="pic" class="form-label">
+          Imagen:
+        </label>
+        <input
+          className="form-control"
+          type="file"
+          placeholder="sube tus fotos"
+          required
+          onChange={(e) => {
+            setPictures(e.target.value);
+          }}
+        ></input>
+        {/* <h2>{productInfo}</h2> */}
+        <button type="submit">Donate</button>
 
-        <button type="submit">Login</button>
-
-        <Link to="/Register">Not member yet? signup here</Link>
+        {/* <Link to="/Register">Not member yet? signup here</Link> */}
       </form>
-    
     </div>
-  )
+  );
 }
 
-export default Donate
+export default Donate;
