cedd25bdeb32972f29f3f8eb2b60b2b30d4c1174
Lunash411 <mkwofiejnr@gmail.com>
2022-09-02 11:17:56 +0200

    product page changes

--- needit/src/components/ProductPage/ProductPage.jsx
+++ needit/src/components/ProductPage/ProductPage.jsx
@@ -2,16 +2,35 @@ import React from 'react'
 import Slider from '../ProductSlider/Slider/Slider'
 
 import Searchbar from '../SearchBar/SearchBar'
-function ProductPage() {
+
+function productApp() {
+  return(
+      <card
+      img = 'https://static.dezeen.com/uploads/2022/05/ella-chair-matthew-hilton-case-furniture-design_dezeen_2364_hero.jpg'
+      description = 'Furniture'
+      />
+  )
+  
+}
+
+function ProductPage(props) {
   return (
     <div className='productPageDiv'>
       <div ><Searchbar/></div>
       {/* /start your code below */}
       <h2>ProductsPage</h2>
+      <div>
+        <img className='card_image'/>
+      </div>
+      <div>
+        <h2 className='card_descrption'></h2>
+        <button className='card_btn'></button>
+      </div>
       <Slider/>
       
     </div>
   )
 }
+
 
 export default ProductPage
---
+++ needit/src/components/ProductPage/productPageDb.js
@@ -0,0 +1,9 @@
+function App() {
+    return(
+        <card
+        img = 'https://static.dezeen.com/uploads/2022/05/ella-chair-matthew-hilton-case-furniture-design_dezeen_2364_hero.jpg'
+        description = 'Furniture'
+        />
+    )
+    
+}
