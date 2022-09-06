16f7be4fd7c0cc4017637f4ee54b67b86d81e722
Lunash411 <mkwofiejnr@gmail.com>
2022-09-06 22:54:08 +0200

    changes

---
+++ server/signup/connect.js
@@ -0,0 +1,4 @@
+const newLocal = "Pi60WLDNGirv0prz2r6QEhroGG2YiTsH";
+  const DBP = newLocal;
+
+ module.exports.DBP ;
--- server/signup/server.js
+++ server/signup/server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const session = require("express-session");
 const flash = require("express-flash");
 const passport = require("passport");
-const {DBP} = require ("./conect.js");
+const {DBP} = require ("./connect.js");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
